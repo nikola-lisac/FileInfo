4c14c1698216b7be4dea30f193e0ee0fe7a3760e
Samir Kahvedzic <samirkafa@gmail.com>
2016-11-17 13:33:26 +0100

    Done countWords

--- src/app/InfoFinder.java
+++ src/app/InfoFinder.java
@@ -29,7 +29,6 @@ public class InfoFinder {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		countSentences(fileName);
 
 	}
 
@@ -40,7 +39,21 @@ public class InfoFinder {
 	 *            ime fajla
 	 */
 	public static void countWords(String fileName) {
+		int count = 0;
+		String line;
+		try (BufferedReader read = Files.newBufferedReader(Paths.get(fileName))) {
+			while ((line = read.readLine()) != null) {
+				System.out.println(line);
+				String[] parts = line.split(" ");
 
+				for (String w : parts) {
+					count++;
+				}
+			}
+			System.out.println("U fajlu se nalazi " + count + " rijeci");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -63,22 +76,12 @@ public class InfoFinder {
 		}
 		System.out.println("U fajlu se nalazi " + count + " recenica");
 
-			
-			
-			
-			
-			
-			
-
 		/**
 		 * Metoda pronalazi i ispisuje broj slova u fajlu
 		 * 
 		 * @param fileName
 		 *            imeFajla
 		 */
-
-
-		
 	}
 
 	public static void countLetters(String fileName) {
